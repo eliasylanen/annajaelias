0cb4eb0d5102de01f630950b3ad7f76bc7a08586
Elias Ylänen <elias.ylanen@gmail.com>
2021-05-23 21:16:51 +0300

    Listen to correct status on registration form

--- src/pages/Register.svelte
+++ src/pages/Register.svelte
@@ -29,9 +29,7 @@
 
     response
       .then(({data, status}) => {
-        console.log({data, status});
-
-        if (status === 200) {
+        if (status === 201) {
           const { token } = data;
 
           localStorage.setItem('token', token);
